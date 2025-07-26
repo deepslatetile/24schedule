04a295617e9a4fad6f1858495e3c08ebbca4a2eb
deepslatetile <155961854+deepslatetile@users.noreply.github.com>
2025-07-26 18:57:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -449,7 +449,7 @@ HTML_TEMPLATE = """
 def icon_for_state(state):
     icons = {
         0: 'https://raw.githubusercontent.com/deepslatetile/24schedule/c5cbe3d8bc5e3028a7872edd61ce78172aba82c9/boarding.png',
-        1: 'https://raw.githubusercontent.com/deepslatetile/24schedule/c5cbe3d8bc5e3028a7872edd61ce78172aba82c9/departure1.png',
+        1: 'https://raw.githubusercontent.com/deepslatetile/24schedule/c5cbe3d8bc5e3028a7872edd61ce78172aba82c9/taxiing.png',
         2: 'https://raw.githubusercontent.com/deepslatetile/24schedule/c5cbe3d8bc5e3028a7872edd61ce78172aba82c9/departure.png',
         3: 'https://raw.githubusercontent.com/deepslatetile/24schedule/c5cbe3d8bc5e3028a7872edd61ce78172aba82c9/cruise.png',
         4: 'https://raw.githubusercontent.com/deepslatetile/24schedule/c5cbe3d8bc5e3028a7872edd61ce78172aba82c9/arrival.png',
@@ -461,7 +461,7 @@ def icon_for_state(state):
 def description_for_state(state):
     descriptions = {
         0: 'Boarding',
-        1: 'Departing',
+        1: 'Taxiing',
         2: 'Climbing',
         3: 'Cruising',
         4: 'Descending',
